314ebb1e90f7e4971d4e8bb88e361740ec812ad8
Ikonnikov Aleksandr <alexandr6519@mail.ru>
2022-11-10 13:30:46 +0700

    We cleared the file from comments

--- MyTests2022/src/TestReadFile.java
+++ MyTests2022/src/TestReadFile.java
@@ -5,31 +5,14 @@ import java.util.Scanner;
 public class TestReadFile {
     public static void main(String[] args) throws FileNotFoundException {
         try (Scanner scanner = new Scanner(new FileInputStream("C:\\Users\\Admin\\IdeaProjects\\untitled\\src\\TEXT\\Test122.txt"))) {
-            StringBuilder sb = new StringBuilder("");
-            //sb = "";
-            String line = "";
-            //String[] array = new String[80];
-            //int i = 0;
+            StringBuilder sb = new StringBuilder();
+
             while (scanner.hasNext()) {
-                line = scanner.nextLine();
-                sb.append(line);
+                sb.append(scanner.nextLine());
                 sb.append(System.lineSeparator());
-
-                //array[i++] = scanner.nextLine();
-
             }
-            //String srt = sb.toString();
+
             System.out.println(sb.toString());
-            /*for(String e: array){
-                if (e != null)
-                System.out.println(e);
-            }
-            System.out.println(Arrays.toString(array));
-             for (int i = 0; i < count; ++i) {
-                array[i] = scanner.nextLine();
-                System.out.println(array[i] + " ");
-
-            }*/
         }
     }
 }
